6941f06ca70c4e49557298a1098f470032a2ca8b
Dave Laing <dave.laing.80@gmail.com>
2020-02-09 15:40:49 +1000

    Prepares for Nix and Travis

---
+++ default.nix
@@ -0,0 +1,37 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+let
+  inherit (nixpkgs) pkgs;
+  haskellPackages = nixpkgs.haskellPackages.override {
+    overrides = self: super: {
+      blog-generator = self.callCabal2nix "blog-generator" ./generator {};
+    };
+  };
+
+  mytexlive = (pkgs.texlive.combine {
+    inherit (pkgs.texlive)
+    prftree
+    scheme-basic
+    collection-latexrecommended;
+  }); 
+
+  content = nixpkgs.stdenv.mkDerivation {
+    name = "blog-content";
+    src = ./content;
+
+    buildPhase = ''
+      export LANG=en_US.UTF-8
+      export LOCALE_ARCHIVE=/run/current-system/sw/lib/locale/locale-archive
+      site build
+    '';
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r _site/* $out/
+    '';
+
+    phases = ["unpackPhase" "buildPhase" "installPhase"];
+
+    buildInputs = [haskellPackages.blog-generator mytexlive pkgs.imagemagick pkgs.ghostscript pkgs.glibcLocales];
+  };
+in
+  content
