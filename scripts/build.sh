57c3f880bf1888fdd279b004ba4ec6753ad06006
Dave Laing <dave.laing.80@gmail.com>
2020-02-09 17:26:22 +1000

    Removes some unused scripts

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cabal new-build
-git submodule init
-git submodule update
-pushd _site && git checkout master && popd
-cabal new-run site -- build  
--- scripts/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cabal new-update
-cabal new-configure
-cabal new-build --only-dependencies
